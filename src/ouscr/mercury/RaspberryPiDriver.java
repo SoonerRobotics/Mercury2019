cdbf52b2cf32b775aec6937e7b852a900e441a83
Noah Zemlin <noah.zemlin@ou.edu>
2019-04-26 19:25:30 -0500

    Stop waiting for other, could be bad

--- src/ouscr/mercury/RaspberryPiDriver.java
+++ src/ouscr/mercury/RaspberryPiDriver.java
@@ -30,8 +30,6 @@ public class RaspberryPiDriver {
         Arduino arduino = new Arduino();
         arduino.open();
 
-        Thread.sleep(500);
-
         Arduino.ArduinoEvent ohaio = new Arduino.ArduinoEvent();
         ohaio.status = 2;
 
